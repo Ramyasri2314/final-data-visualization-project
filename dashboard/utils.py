fd9bcfaeec6c75d4b224753a03a9d314d9595588
Ramya sri <ramyasri23014@gmail.com>
2026-07-29 00:39:02 +0200

    Fix dataset path

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -1,11 +1,14 @@
+from pathlib import Path
 import pandas as pd
 import streamlit as st
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/space_missions.csv", encoding="latin1")
+    csv_path = Path(__file__).parent / "data" / "space_missions.csv"
 
-    df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
+    df = pd.read_csv(csv_path, encoding="latin1")
+
+    df["Date"] = pd.to_datetime(df["Date"])
     df["Year"] = df["Date"].dt.year
 
     return df
